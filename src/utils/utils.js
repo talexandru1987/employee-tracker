ab05bdef6aa8c8b3bdc73f6e799e0ca849cc5f1c
Alexandru <T_alexandru87@yahoo.com>
2022-08-06 09:13:46 +0100

    changed variable names in utils forlder

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,9 +1,9 @@
 //select all from a table
-const selectAll = (aTable) => `SELECT * FROM ${aTable}`;
+const selectAll = (table) => `SELECT * FROM ${table}`;
 
 //select all roles
-const selectAllRoles = (aTable, columns, aGroup) =>
-  `SELECT ${columns} FROM ${aTable} GROUP BY ${aGroup}`;
+const selectAllRoles = (table, columns, aGroup) =>
+  `SELECT ${columns} FROM ${table} GROUP BY ${aGroup}`;
 
 //select all from a table
 const selectAllEmployees =
@@ -14,11 +14,11 @@ JOIN role r ON  e.role_id = r.id
 JOIN department d ON r.department_id = d.id`;
 
 //select by department
-const selectDepartmentEmployees = (aDepartment) => `SELECT * 
+const selectDepartmentEmployees = (department) => `SELECT * 
 FROM employee e, department d, role r
 WHERE e.role_id = r.id 
 and  r.department_id = d.id
-and d.name = '${aDepartment}'`;
+and d.name = '${department}'`;
 
 // select by manager
 const selectManagerEmployees = (managerId) => `SELECT * 
@@ -26,43 +26,41 @@ FROM employee_db.employee e, employee_db.role r
 WHERE e.role_id = r.id 
 and e.manager_id = '${managerId}'`;
 //select department budget
-const selectDepartmentBudget = (aDepartment) => `SELECT sum(salary)
+const selectDepartmentBudget = (department) => `SELECT sum(salary)
 FROM employee e, department d, role r
 WHERE e.role_id = r.id 
 and  r.department_id = d.id
-and d.name = '${aDepartment}'`;
+and d.name = '${department}'`;
 
 //add department
-const addAdepartment = (anId, aName) =>
-  `INSERT INTO department (id, name) VALUES ('${anId}', '${aName}')`;
+const addAdepartment = (id, aName) =>
+  `INSERT INTO department (id, name) VALUES ('${id}', '${aName}')`;
 
 // get max id
-const maxId = (aDepartment) => `SELECT max(id) as max FROM ${aDepartment}`;
+const maxId = (department) => `SELECT max(id) as max FROM ${department}`;
 
 //add role
-const addArole = (anId, aTitle, aSalary, aDepartment_id) =>
-  `INSERT INTO role (id, title, salary, department_id) VALUES ('${anId}', '${aTitle}','${aSalary}','${aDepartment_id}')`;
+const addArole = (id, aTitle, aSalary, department_id) =>
+  `INSERT INTO role (id, title, salary, department_id) VALUES ('${id}', '${aTitle}','${aSalary}','${department_id}')`;
 
 //get specific department if
-const specificDepartment = (aDepartment) =>
-  `SELECT id FROM department WHERE name = '${aDepartment}'`;
+const specificDepartment = (department) => `SELECT id FROM department WHERE name = '${department}'`;
 
 //get all employees by role
-const employeeByRole = (aRole) => `SELECT e.id, first_name 
+const employeeByRole = (role) => `SELECT e.id, first_name 
   FROM employee e, role r
   WHERE e.role_id = r.id 
-  and r.title = '${aRole}';`;
+  and r.title = '${role}';`;
 
 //add an employee
-const addAnEmployee = (anId, fName, lName, rId, mId) =>
-  `INSERT INTO employee (id, first_name, last_name, role_id, manager_id) VALUES ('${anId}', '${fName}','${lName}','${rId}','${mId}')`;
+const addAnEmployee = (id, fName, lName, rId, mId) =>
+  `INSERT INTO employee (id, first_name, last_name, role_id, manager_id) VALUES ('${id}', '${fName}','${lName}','${rId}','${mId}')`;
 
 //update an employee role
-const updateEmployeeDetails = (aColumn, aValue, anId) =>
-  `UPDATE employee SET ${aColumn} = '${aValue}' WHERE id = '${anId}'`;
+const updateEmployeeDetails = (aColumn, aValue, id) =>
+  `UPDATE employee SET ${aColumn} = '${aValue}' WHERE id = '${id}'`;
 
-const deleteAnOption = (aTable, aValue, anId) =>
-  `DELETE FROM ${aTable} WHERE ${aValue} = '${anId}'`;
+const deleteAnOption = (table, aValue, id) => `DELETE FROM ${table} WHERE ${aValue} = '${id}'`;
 
 module.exports = {
   selectAll,
